9473a40cc6d9dfdcdbf3bf270099a2b4fa88ebcd
Jorge <jorgediazok@gmail.com>
2020-05-01 03:16:35 -0300

    more changes

---
+++ app.js
@@ -0,0 +1,36 @@
+//Global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+//Functions
+
+//Color generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+    //Add the color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    //Check for Contrast
+    checkTextContrasts(randomColor, hexText);
+  });
+}
+
+function checkTextContrasts(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+  }
+}
+randomColors();
